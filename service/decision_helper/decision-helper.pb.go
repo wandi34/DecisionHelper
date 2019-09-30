a6d39bf6e19b060810e1ea366eff8442a333f9b8
Andreas Winter <awi@adorsys.de>
2019-09-30 13:54:32 +0200

    Add vote function in BE

--- service/decision_helper/decision-helper.pb.go
+++ service/decision_helper/decision-helper.pb.go
@@ -24,7 +24,124 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
-// The Topics Request
+type Topic struct {
+	Id                   int32    `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	Title                string   `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Topic) Reset()         { *m = Topic{} }
+func (m *Topic) String() string { return proto.CompactTextString(m) }
+func (*Topic) ProtoMessage()    {}
+func (*Topic) Descriptor() ([]byte, []int) {
+	return fileDescriptor_30e41342fbb1d60d, []int{0}
+}
+
+func (m *Topic) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Topic.Unmarshal(m, b)
+}
+func (m *Topic) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Topic.Marshal(b, m, deterministic)
+}
+func (m *Topic) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Topic.Merge(m, src)
+}
+func (m *Topic) XXX_Size() int {
+	return xxx_messageInfo_Topic.Size(m)
+}
+func (m *Topic) XXX_DiscardUnknown() {
+	xxx_messageInfo_Topic.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Topic proto.InternalMessageInfo
+
+func (m *Topic) GetId() int32 {
+	if m != nil {
+		return m.Id
+	}
+	return 0
+}
+
+func (m *Topic) GetTitle() string {
+	if m != nil {
+		return m.Title
+	}
+	return ""
+}
+
+type Decision struct {
+	Id                   int32    `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	TopicId              int32    `protobuf:"varint,2,opt,name=topicId,proto3" json:"topicId,omitempty"`
+	Title                string   `protobuf:"bytes,3,opt,name=title,proto3" json:"title,omitempty"`
+	Hots                 int32    `protobuf:"varint,4,opt,name=hots,proto3" json:"hots,omitempty"`
+	Nots                 int32    `protobuf:"varint,5,opt,name=nots,proto3" json:"nots,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Decision) Reset()         { *m = Decision{} }
+func (m *Decision) String() string { return proto.CompactTextString(m) }
+func (*Decision) ProtoMessage()    {}
+func (*Decision) Descriptor() ([]byte, []int) {
+	return fileDescriptor_30e41342fbb1d60d, []int{1}
+}
+
+func (m *Decision) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Decision.Unmarshal(m, b)
+}
+func (m *Decision) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Decision.Marshal(b, m, deterministic)
+}
+func (m *Decision) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Decision.Merge(m, src)
+}
+func (m *Decision) XXX_Size() int {
+	return xxx_messageInfo_Decision.Size(m)
+}
+func (m *Decision) XXX_DiscardUnknown() {
+	xxx_messageInfo_Decision.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Decision proto.InternalMessageInfo
+
+func (m *Decision) GetId() int32 {
+	if m != nil {
+		return m.Id
+	}
+	return 0
+}
+
+func (m *Decision) GetTopicId() int32 {
+	if m != nil {
+		return m.TopicId
+	}
+	return 0
+}
+
+func (m *Decision) GetTitle() string {
+	if m != nil {
+		return m.Title
+	}
+	return ""
+}
+
+func (m *Decision) GetHots() int32 {
+	if m != nil {
+		return m.Hots
+	}
+	return 0
+}
+
+func (m *Decision) GetNots() int32 {
+	if m != nil {
+		return m.Nots
+	}
+	return 0
+}
+
 type TopicsRequest struct {
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
@@ -35,7 +152,7 @@ func (m *TopicsRequest) Reset()         { *m = TopicsRequest{} }
 func (m *TopicsRequest) String() string { return proto.CompactTextString(m) }
 func (*TopicsRequest) ProtoMessage()    {}
 func (*TopicsRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_30e41342fbb1d60d, []int{0}
+	return fileDescriptor_30e41342fbb1d60d, []int{2}
 }
 
 func (m *TopicsRequest) XXX_Unmarshal(b []byte) error {
@@ -56,9 +173,8 @@ func (m *TopicsRequest) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_TopicsRequest proto.InternalMessageInfo
 
-// The response message containing the greetings
 type TopicsReply struct {
-	Topics               []string `protobuf:"bytes,1,rep,name=topics,proto3" json:"topics,omitempty"`
+	Topics               []*Topic `protobuf:"bytes,1,rep,name=topics,proto3" json:"topics,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -68,7 +184,7 @@ func (m *TopicsReply) Reset()         { *m = TopicsReply{} }
 func (m *TopicsReply) String() string { return proto.CompactTextString(m) }
 func (*TopicsReply) ProtoMessage()    {}
 func (*TopicsReply) Descriptor() ([]byte, []int) {
-	return fileDescriptor_30e41342fbb1d60d, []int{1}
+	return fileDescriptor_30e41342fbb1d60d, []int{3}
 }
 
 func (m *TopicsReply) XXX_Unmarshal(b []byte) error {
@@ -89,16 +205,15 @@ func (m *TopicsReply) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_TopicsReply proto.InternalMessageInfo
 
-func (m *TopicsReply) GetTopics() []string {
+func (m *TopicsReply) GetTopics() []*Topic {
 	if m != nil {
 		return m.Topics
 	}
 	return nil
 }
 
-// The response message containing the decisions for a topic
 type DecisionsRequest struct {
-	Topic                string   `protobuf:"bytes,1,opt,name=topic,proto3" json:"topic,omitempty"`
+	TopicId              int32    `protobuf:"varint,1,opt,name=topicId,proto3" json:"topicId,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -108,7 +223,7 @@ func (m *DecisionsRequest) Reset()         { *m = DecisionsRequest{} }
 func (m *DecisionsRequest) String() string { return proto.CompactTextString(m) }
 func (*DecisionsRequest) ProtoMessage()    {}
 func (*DecisionsRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_30e41342fbb1d60d, []int{2}
+	return fileDescriptor_30e41342fbb1d60d, []int{4}
 }
 
 func (m *DecisionsRequest) XXX_Unmarshal(b []byte) error {
@@ -129,26 +244,25 @@ func (m *DecisionsRequest) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_DecisionsRequest proto.InternalMessageInfo
 
-func (m *DecisionsRequest) GetTopic() string {
+func (m *DecisionsRequest) GetTopicId() int32 {
 	if m != nil {
-		return m.Topic
+		return m.TopicId
 	}
-	return ""
+	return 0
 }
 
-// The response message containing the decisions for a topic
 type DecisionsReply struct {
-	Decisions            []string `protobuf:"bytes,1,rep,name=decisions,proto3" json:"decisions,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
+	Decisions            []*Decision `protobuf:"bytes,1,rep,name=decisions,proto3" json:"decisions,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}    `json:"-"`
+	XXX_unrecognized     []byte      `json:"-"`
+	XXX_sizecache        int32       `json:"-"`
 }
 
 func (m *DecisionsReply) Reset()         { *m = DecisionsReply{} }
 func (m *DecisionsReply) String() string { return proto.CompactTextString(m) }
 func (*DecisionsReply) ProtoMessage()    {}
 func (*DecisionsReply) Descriptor() ([]byte, []int) {
-	return fileDescriptor_30e41342fbb1d60d, []int{3}
+	return fileDescriptor_30e41342fbb1d60d, []int{5}
 }
 
 func (m *DecisionsReply) XXX_Unmarshal(b []byte) error {
@@ -169,40 +283,138 @@ func (m *DecisionsReply) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_DecisionsReply proto.InternalMessageInfo
 
-func (m *DecisionsReply) GetDecisions() []string {
+func (m *DecisionsReply) GetDecisions() []*Decision {
 	if m != nil {
 		return m.Decisions
 	}
 	return nil
 }
 
+type VoteRequest struct {
+	TopicId              int32    `protobuf:"varint,1,opt,name=topicId,proto3" json:"topicId,omitempty"`
+	DecisionId           int32    `protobuf:"varint,2,opt,name=decisionId,proto3" json:"decisionId,omitempty"`
+	Hot                  bool     `protobuf:"varint,3,opt,name=hot,proto3" json:"hot,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *VoteRequest) Reset()         { *m = VoteRequest{} }
+func (m *VoteRequest) String() string { return proto.CompactTextString(m) }
+func (*VoteRequest) ProtoMessage()    {}
+func (*VoteRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_30e41342fbb1d60d, []int{6}
+}
+
+func (m *VoteRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_VoteRequest.Unmarshal(m, b)
+}
+func (m *VoteRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_VoteRequest.Marshal(b, m, deterministic)
+}
+func (m *VoteRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_VoteRequest.Merge(m, src)
+}
+func (m *VoteRequest) XXX_Size() int {
+	return xxx_messageInfo_VoteRequest.Size(m)
+}
+func (m *VoteRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_VoteRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_VoteRequest proto.InternalMessageInfo
+
+func (m *VoteRequest) GetTopicId() int32 {
+	if m != nil {
+		return m.TopicId
+	}
+	return 0
+}
+
+func (m *VoteRequest) GetDecisionId() int32 {
+	if m != nil {
+		return m.DecisionId
+	}
+	return 0
+}
+
+func (m *VoteRequest) GetHot() bool {
+	if m != nil {
+		return m.Hot
+	}
+	return false
+}
+
+type VoteReply struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *VoteReply) Reset()         { *m = VoteReply{} }
+func (m *VoteReply) String() string { return proto.CompactTextString(m) }
+func (*VoteReply) ProtoMessage()    {}
+func (*VoteReply) Descriptor() ([]byte, []int) {
+	return fileDescriptor_30e41342fbb1d60d, []int{7}
+}
+
+func (m *VoteReply) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_VoteReply.Unmarshal(m, b)
+}
+func (m *VoteReply) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_VoteReply.Marshal(b, m, deterministic)
+}
+func (m *VoteReply) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_VoteReply.Merge(m, src)
+}
+func (m *VoteReply) XXX_Size() int {
+	return xxx_messageInfo_VoteReply.Size(m)
+}
+func (m *VoteReply) XXX_DiscardUnknown() {
+	xxx_messageInfo_VoteReply.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_VoteReply proto.InternalMessageInfo
+
 func init() {
+	proto.RegisterType((*Topic)(nil), "decision_helper.Topic")
+	proto.RegisterType((*Decision)(nil), "decision_helper.Decision")
 	proto.RegisterType((*TopicsRequest)(nil), "decision_helper.TopicsRequest")
 	proto.RegisterType((*TopicsReply)(nil), "decision_helper.TopicsReply")
 	proto.RegisterType((*DecisionsRequest)(nil), "decision_helper.DecisionsRequest")
 	proto.RegisterType((*DecisionsReply)(nil), "decision_helper.DecisionsReply")
+	proto.RegisterType((*VoteRequest)(nil), "decision_helper.VoteRequest")
+	proto.RegisterType((*VoteReply)(nil), "decision_helper.VoteReply")
 }
 
 func init() { proto.RegisterFile("decision-helper.proto", fileDescriptor_30e41342fbb1d60d) }
 
 var fileDescriptor_30e41342fbb1d60d = []byte{
-	// 245 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x4d, 0x49, 0x4d, 0xce,
-	0x2c, 0xce, 0xcc, 0xcf, 0xd3, 0xcd, 0x48, 0xcd, 0x29, 0x48, 0x2d, 0xd2, 0x2b, 0x28, 0xca, 0x2f,
-	0xc9, 0x17, 0xe2, 0x87, 0x09, 0xc7, 0x43, 0x84, 0x95, 0xf8, 0xb9, 0x78, 0x43, 0xf2, 0x0b, 0x32,
-	0x93, 0x8b, 0x83, 0x52, 0x0b, 0x4b, 0x53, 0x8b, 0x4b, 0x94, 0x54, 0xb9, 0xb8, 0x61, 0x02, 0x05,
-	0x39, 0x95, 0x42, 0x62, 0x5c, 0x6c, 0x25, 0x60, 0xae, 0x04, 0xa3, 0x02, 0xb3, 0x06, 0x67, 0x10,
-	0x94, 0xa7, 0xa4, 0xc1, 0x25, 0xe0, 0x02, 0x35, 0x0a, 0xa6, 0x55, 0x48, 0x84, 0x8b, 0x15, 0x2c,
-	0x2b, 0xc1, 0xa8, 0xc0, 0xa8, 0xc1, 0x19, 0x04, 0xe1, 0x28, 0xe9, 0x71, 0xf1, 0x21, 0xa9, 0x04,
-	0x99, 0x29, 0xc3, 0xc5, 0x09, 0x73, 0x06, 0xcc, 0x58, 0x84, 0x80, 0xd1, 0x6e, 0x46, 0x84, 0x06,
-	0x0f, 0xb0, 0x23, 0x85, 0xbc, 0xb9, 0x38, 0xdd, 0x53, 0x4b, 0x20, 0xce, 0x12, 0x92, 0xd3, 0x43,
-	0xf3, 0x83, 0x1e, 0x8a, 0x07, 0xa4, 0x64, 0x70, 0xca, 0x17, 0xe4, 0x54, 0x2a, 0x31, 0x08, 0xc5,
-	0x70, 0x89, 0xb8, 0xa7, 0x96, 0xc0, 0x9d, 0xe4, 0x96, 0x5f, 0x04, 0x96, 0x17, 0x52, 0xc4, 0xd0,
-	0x87, 0xee, 0x41, 0x29, 0x79, 0x7c, 0x4a, 0xc0, 0xa6, 0x3b, 0x99, 0x70, 0x49, 0x67, 0xe6, 0xeb,
-	0xa5, 0x17, 0x15, 0x24, 0xeb, 0xa5, 0x56, 0x24, 0xe6, 0x16, 0xe4, 0xa4, 0x16, 0xeb, 0x65, 0xa4,
-	0xe6, 0xe4, 0xe4, 0x97, 0xe7, 0x17, 0xe5, 0xa4, 0x38, 0x09, 0xa3, 0xfa, 0x2c, 0x00, 0x14, 0x29,
-	0x01, 0x8c, 0x49, 0x6c, 0xe0, 0xd8, 0x31, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0xa8, 0x66, 0x85,
-	0x60, 0xb6, 0x01, 0x00, 0x00,
+	// 384 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x53, 0x41, 0x4b, 0xfb, 0x30,
+	0x1c, 0xfd, 0xb7, 0x5b, 0xf7, 0x5f, 0x7f, 0xc5, 0x6d, 0xc4, 0x29, 0x75, 0xca, 0x9c, 0x39, 0xed,
+	0xe0, 0x7a, 0x98, 0x82, 0x27, 0x2f, 0x43, 0x36, 0x87, 0x08, 0xa3, 0x88, 0x20, 0x08, 0xa2, 0x6b,
+	0xb0, 0x85, 0xb8, 0xc4, 0x36, 0xa2, 0xfb, 0x0a, 0x7e, 0x6a, 0x49, 0xb2, 0x74, 0x9d, 0x73, 0x7a,
+	0x4b, 0x5e, 0xde, 0xef, 0xbd, 0x97, 0x47, 0x02, 0x3b, 0x11, 0x99, 0x26, 0x59, 0xc2, 0x66, 0xbd,
+	0x98, 0x50, 0x4e, 0xd2, 0x80, 0xa7, 0x4c, 0x30, 0x54, 0x37, 0xf0, 0x83, 0x86, 0x71, 0x0f, 0x9c,
+	0x1b, 0xc6, 0x93, 0x29, 0xaa, 0x81, 0x9d, 0x44, 0xbe, 0xd5, 0xb1, 0xba, 0x4e, 0x68, 0x27, 0x11,
+	0x6a, 0x82, 0x23, 0x12, 0x41, 0x89, 0x6f, 0x77, 0xac, 0xae, 0x1b, 0xea, 0x0d, 0x4e, 0xa1, 0x7a,
+	0xb1, 0x50, 0x58, 0x9b, 0xf0, 0xe1, 0xbf, 0x90, 0x52, 0xe3, 0x48, 0xcd, 0x38, 0xa1, 0xd9, 0x2e,
+	0xb5, 0x4a, 0x05, 0x2d, 0x84, 0xa0, 0x1c, 0x33, 0x91, 0xf9, 0x65, 0x45, 0x56, 0x6b, 0x89, 0xcd,
+	0x24, 0xe6, 0x68, 0x4c, 0xae, 0x71, 0x1d, 0xb6, 0x54, 0xc4, 0x2c, 0x24, 0xaf, 0x6f, 0x24, 0x13,
+	0xf8, 0x1c, 0x3c, 0x03, 0x70, 0x3a, 0x47, 0x01, 0x54, 0x94, 0x51, 0xe6, 0x5b, 0x9d, 0x52, 0xd7,
+	0xeb, 0xef, 0x06, 0xdf, 0x2e, 0x19, 0x28, 0x76, 0xb8, 0x60, 0xe1, 0x63, 0x68, 0x98, 0x3b, 0x18,
+	0xc9, 0x62, 0x76, 0x6b, 0x25, 0x3b, 0x1e, 0x43, 0xad, 0xc0, 0x96, 0x7e, 0x67, 0xe0, 0x1a, 0x03,
+	0x63, 0xb9, 0xb7, 0x66, 0x69, 0x66, 0xc2, 0x25, 0x17, 0xdf, 0x81, 0x77, 0xcb, 0x04, 0xf9, 0xd3,
+	0x13, 0xb5, 0x01, 0xcc, 0x54, 0x5e, 0x66, 0x01, 0x41, 0x0d, 0x28, 0xc5, 0x4c, 0xa8, 0x36, 0xab,
+	0xa1, 0x5c, 0x62, 0x0f, 0x5c, 0x2d, 0xcd, 0xe9, 0xbc, 0xff, 0x69, 0x2f, 0x33, 0x5f, 0xaa, 0x38,
+	0xe8, 0x0a, 0xdc, 0x11, 0x11, 0xba, 0x35, 0xd4, 0xfe, 0xb9, 0x20, 0x53, 0x46, 0xeb, 0x60, 0xe3,
+	0x39, 0xa7, 0x73, 0xfc, 0x0f, 0xdd, 0x43, 0x73, 0x44, 0x44, 0xde, 0xca, 0x90, 0xa5, 0xfa, 0x09,
+	0x1d, 0x6d, 0x6c, 0x21, 0x97, 0x3e, 0xfc, 0x8d, 0xa2, 0xd5, 0xaf, 0xa1, 0x2e, 0xaf, 0x32, 0x64,
+	0x69, 0xfe, 0xd2, 0xd6, 0x03, 0x15, 0x7a, 0x6c, 0xb5, 0x36, 0x9c, 0x2a, 0xb9, 0xc1, 0x29, 0xec,
+	0x27, 0x2c, 0x78, 0x4e, 0xf9, 0x34, 0x20, 0x1f, 0x8f, 0x2f, 0x9c, 0x92, 0x2c, 0x88, 0x09, 0xa5,
+	0xec, 0x9d, 0xa5, 0x34, 0x1a, 0x6c, 0xaf, 0x16, 0x35, 0x91, 0xbf, 0x64, 0x62, 0x3d, 0x55, 0xd4,
+	0x77, 0x39, 0xf9, 0x0a, 0x00, 0x00, 0xff, 0xff, 0x9d, 0xee, 0xd3, 0x34, 0x47, 0x03, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -220,6 +432,7 @@ type DecisionHelperClient interface {
 	// Returns topics
 	GetTopics(ctx context.Context, in *TopicsRequest, opts ...grpc.CallOption) (*TopicsReply, error)
 	GetDecisionsForTopic(ctx context.Context, in *DecisionsRequest, opts ...grpc.CallOption) (*DecisionsReply, error)
+	VoteForDecision(ctx context.Context, in *VoteRequest, opts ...grpc.CallOption) (*VoteReply, error)
 }
 
 type decisionHelperClient struct {
@@ -248,11 +461,21 @@ func (c *decisionHelperClient) GetDecisionsForTopic(ctx context.Context, in *Dec
 	return out, nil
 }
 
+func (c *decisionHelperClient) VoteForDecision(ctx context.Context, in *VoteRequest, opts ...grpc.CallOption) (*VoteReply, error) {
+	out := new(VoteReply)
+	err := c.cc.Invoke(ctx, "/decision_helper.DecisionHelper/VoteForDecision", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // DecisionHelperServer is the server API for DecisionHelper service.
 type DecisionHelperServer interface {
 	// Returns topics
 	GetTopics(context.Context, *TopicsRequest) (*TopicsReply, error)
 	GetDecisionsForTopic(context.Context, *DecisionsRequest) (*DecisionsReply, error)
+	VoteForDecision(context.Context, *VoteRequest) (*VoteReply, error)
 }
 
 // UnimplementedDecisionHelperServer can be embedded to have forward compatible implementations.
@@ -264,6 +487,9 @@ func (*UnimplementedDecisionHelperServer) GetTopics(ctx context.Context, req *To
 }
 func (*UnimplementedDecisionHelperServer) GetDecisionsForTopic(ctx context.Context, req *DecisionsRequest) (*DecisionsReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetDecisionsForTopic not implemented")
+}
+func (*UnimplementedDecisionHelperServer) VoteForDecision(ctx context.Context, req *VoteRequest) (*VoteReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method VoteForDecision not implemented")
 }
 
 func RegisterDecisionHelperServer(s *grpc.Server, srv DecisionHelperServer) {
@@ -306,6 +532,24 @@ func _DecisionHelper_GetDecisionsForTopic_Handler(srv interface{}, ctx context.C
 	return interceptor(ctx, in, info, handler)
 }
 
+func _DecisionHelper_VoteForDecision_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(VoteRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DecisionHelperServer).VoteForDecision(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/decision_helper.DecisionHelper/VoteForDecision",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DecisionHelperServer).VoteForDecision(ctx, req.(*VoteRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _DecisionHelper_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "decision_helper.DecisionHelper",
 	HandlerType: (*DecisionHelperServer)(nil),
@@ -317,6 +561,10 @@ var _DecisionHelper_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetDecisionsForTopic",
 			Handler:    _DecisionHelper_GetDecisionsForTopic_Handler,
+		},
+		{
+			MethodName: "VoteForDecision",
+			Handler:    _DecisionHelper_VoteForDecision_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
